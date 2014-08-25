480007fbe701e4f843ef7ca0c1f4d67f695321a1
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2014-08-25 16:58:09 +0200

    Finer control over tracing

--- src/marlowe-trace.adb
+++ src/marlowe-trace.adb
@@ -1,15 +1,47 @@
+with Ada.Calendar;
+with Ada.Task_Identification;
 with Ada.Text_IO;
 
 package body Marlowe.Trace is
+
+   File       : Ada.Text_IO.File_Type;
+   Start_Time : Ada.Calendar.Time;
+
+   ---------------
+   -- End_Trace --
+   ---------------
+
+   procedure End_Trace is
+   begin
+      Ada.Text_IO.Close (File);
+   end End_Trace;
+
+   -----------------
+   -- Start_Trace --
+   -----------------
+
+   procedure Start_Trace (File_Name : String) is
+   begin
+      Ada.Text_IO.Create (File, Ada.Text_IO.Out_File, File_Name);
+      Start_Time := Ada.Calendar.Clock;
+   end Start_Trace;
 
    -----------
    -- Trace --
    -----------
 
    procedure Trace (Message : String) is
+      use Ada.Calendar;
+      Time_Stamp : constant Duration :=
+                     Ada.Calendar.Clock - Start_Time;
    begin
-      Ada.Text_IO.Put_Line (Message);
-      Ada.Text_IO.Flush;
+      Ada.Text_IO.Put_Line
+        (File,
+         Duration'Image (Time_Stamp)
+         & ": "
+         & Ada.Task_Identification.Image
+           (Ada.Task_Identification.Current_Task)
+         & ": " & Message);
    end Trace;
 
 end Marlowe.Trace;
--- src/marlowe-trace.ads
+++ src/marlowe-trace.ads
@@ -2,6 +2,9 @@ package Marlowe.Trace is
 
    Tracing : Boolean := False;
 
+   procedure Start_Trace (File_Name : String);
+   procedure End_Trace;
+
    procedure Trace (Message : String);
 
 end Marlowe.Trace;
